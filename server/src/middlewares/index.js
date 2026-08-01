10565f772f8efe9dabaf43a0b4c62d6ed5734899
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-08-01 10:53:19 +0530

    feat: add optional middleware

--- server/src/middlewares/index.js
+++ server/src/middlewares/index.js
@@ -1,3 +1,4 @@
 export { default as errorHandler } from "./error.middleware.js"
 export { default as validationResultMiddleware } from "./validation.result.middleware.js"
 export { default as authMiddleware } from "./auth.middleware.js"
+export { default as optionalMiddleware } from "./optional.middleware.js"
