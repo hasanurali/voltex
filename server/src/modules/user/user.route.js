dd9300a31e094dcba0e0dd9818bb327b6af5e8fd
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-08-24 11:13:24 +0530

    feat: add user status checking functionality

--- server/src/modules/user/user.route.js
+++ server/src/modules/user/user.route.js
@@ -2,10 +2,16 @@ import express from "express";
 const userRoutes = express.Router();
 
 import * as controllers from "./user.controller.js";
+import * as middlewares from "../../middlewares/index.js"
 
 
 userRoutes.get("/",
     controllers.fetchUsersController,
+);
+
+userRoutes.post("/user-statuses",
+    middlewares.authMiddleware,
+    controllers.checkUserStatusesController
 );
 
 
--- server/src/shared/constants/messages/user.messages.js
+++ server/src/shared/constants/messages/user.messages.js
@@ -1,7 +1,9 @@
 const USER_MESSAGES = Object.freeze({
     FETCH_SUCCESS: "Users fetched successfully",
+    USER_STATUSES_FETCH_SUCCESS: "User statuses fetched successfully",
 
     SEARCH_REQUIRED: "Please enter a search term",
+    INVALID_ARRAY_FORMAT: "User ids must be an array",
     NOT_FOUND: "User not found"
 });
 
