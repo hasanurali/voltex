e7489fcd940eb6723d5443f6c9d1ddd155b396a1
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-08-29 10:16:21 +0530

    feat: add error boundary and route error handling

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
 import AppProviders from './AppProviders.tsx';
+import { ErrorBoundary } from "./ErrorBoundary.tsx";
 
 const App = () => {
   return (
-    <AppProviders>
-      <RouterProvider router={router} />
-    </AppProviders>
+    <ErrorBoundary>
+      <AppProviders>
+        <RouterProvider router={router} />
+      </AppProviders>
+    </ErrorBoundary>
   );
 };
 
