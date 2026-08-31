515144173c3fcc133d84c44eda65906f531123b4
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-08-31 11:09:45 +0530

    feat: add password visibility toggle to Input component

--- client/src/components/common/Input.tsx
+++ client/src/components/common/Input.tsx
@@ -1,16 +1,32 @@
-import type { InputHTMLAttributes } from 'react';
+import { useState, type InputHTMLAttributes } from 'react';
+import { Eye, EyeOff } from 'lucide-react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    showPasswordToggle?: boolean,
     error?: string;
-}
+};
 
-const Input = ({ className = '', error, ...props }: InputProps) => {
+const Input = ({ className = '', showPasswordToggle = false, type = 'text', error, ...props }: InputProps) => {
+
+    const [passwordToggle, setPasswordToggle] = useState<boolean>(false);
+
     return (
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 relative">
+
             <input
-                className={`rounded-md border px-3 py-2 font-sans text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-primary-500 disabled:opacity-50 disabled:cursor-not-allowed ${error ? 'border-danger' : 'border-border'} ${className}`}
+                type={showPasswordToggle ? (passwordToggle ? 'text' : 'password') : type}
+                className={`rounded-md border px-3 py-2 ${showPasswordToggle && 'pr-12 '} font-sans text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-primary-500 disabled:opacity-50 disabled:cursor-not-allowed ${error ? 'border-danger' : 'border-border'} ${className}`}
                 {...props}
             />
+
+            {showPasswordToggle && <button
+                type="button"
+                className='absolute right-3.5 top-3.25'
+                onClick={() => setPasswordToggle(!passwordToggle)}
+            >
+                {passwordToggle ? <Eye color='gray' /> : <EyeOff color='gray' />}
+            </button>}
+
             {error && <span className="text-sm text-danger">{error}</span>}
         </div>
     );
