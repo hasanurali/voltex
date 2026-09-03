20639ea6f1c88e00fd62776afa7bb32abfd98460
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-09-03 09:35:56 +0530

    feat: add nested router structure with guest route guard

--- client/src/app/router.tsx
+++ client/src/app/router.tsx
@@ -2,11 +2,32 @@ import { createBrowserRouter } from 'react-router-dom';
 import { ROUTES } from './routes.ts';
 import NotFoundPage from '../pages/NotFoundPage.tsx';
 import RouteErrorPage from "../pages/RouteErrorPage.tsx"
+import GuestRoute from './GuestRoute.tsx';
+import { LoginPage } from '@/features/auth';
+
 
 export const router = createBrowserRouter([
     {
-        path: ROUTES.notFound,
-        element: <NotFoundPage />,
-        errorElement: <RouteErrorPage />
+        path: '/',
+        errorElement: <RouteErrorPage />,
+        children: [
+
+            // Guest routes
+            {
+                element: <GuestRoute />,
+                children: [
+                    {
+                        path: ROUTES.login,
+                        element: <LoginPage />
+                    }
+                ],
+            },
+
+            // Public routes
+            {
+                path: ROUTES.notFound,
+                element: <NotFoundPage />
+            }
+        ],
     },
 ]);
