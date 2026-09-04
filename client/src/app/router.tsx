80a803a4647024fc2d677659c4b7b0f80069d587
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-09-04 09:40:25 +0530

    feat: add VerifyEmailPage with wireing verify-email route

--- client/src/app/router.tsx
+++ client/src/app/router.tsx
@@ -3,7 +3,7 @@ import { ROUTES } from './routes.ts';
 import NotFoundPage from '../pages/NotFoundPage.tsx';
 import RouteErrorPage from "../pages/RouteErrorPage.tsx"
 import GuestRoute from './GuestRoute.tsx';
-import { LoginPage, RegisterPage } from '@/features/auth';
+import { LoginPage, RegisterPage, VerifyEmailPage } from '@/features/auth';
 
 
 export const router = createBrowserRouter([
@@ -23,6 +23,10 @@ export const router = createBrowserRouter([
                     {
                         path: ROUTES.register,
                         element: <RegisterPage />
+                    },
+                    {
+                        path: ROUTES.verifyEmail,
+                        element: <VerifyEmailPage />
                     }
                 ],
             },
--- client/src/features/auth/index.ts
+++ client/src/features/auth/index.ts
@@ -1,2 +1,3 @@
 export { default as LoginPage } from './pages/LoginPage';
 export { default as RegisterPage } from './pages/RegisterPage';
+export { default as VerifyEmailPage } from './pages/VerifyEmailPage';
