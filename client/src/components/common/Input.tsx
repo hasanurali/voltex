bfe1047ef7b58099bffd11a23dc1252bcf1a4976
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-09-04 08:29:36 +0530

    feat: add ref support to Input

--- client/src/components/common/Input.tsx
+++ client/src/components/common/Input.tsx
@@ -1,12 +1,13 @@
-import { useState, type InputHTMLAttributes } from 'react';
+import { useState, type InputHTMLAttributes, type Ref } from 'react';
 import { Eye, EyeOff } from 'lucide-react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     showPasswordToggle?: boolean,
+    ref?: Ref<HTMLInputElement>;
     error?: string;
 };
 
-const Input = ({ className = '', showPasswordToggle = false, type = 'text', error, ...props }: InputProps) => {
+const Input = ({ className = '', showPasswordToggle = false, type = 'text', ref, error, ...props }: InputProps) => {
 
     const [passwordToggle, setPasswordToggle] = useState<boolean>(false);
 
@@ -14,6 +15,7 @@ const Input = ({ className = '', showPasswordToggle = false, type = 'text', erro
         <div className="flex flex-col relative">
 
             <input
+                ref={ref}
                 type={showPasswordToggle ? (passwordToggle ? 'text' : 'password') : type}
                 className={`rounded-md border px-3 py-2 ${showPasswordToggle && 'pr-12 '} font-sans text-foreground placeholder:text-muted-foreground focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed ${error ? 'border-danger' : 'border-border'} ${className}`}
                 {...props}
