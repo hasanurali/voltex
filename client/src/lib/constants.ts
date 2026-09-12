f6b3e4ad58e6b44700e12d9adb16428bcd8c9850
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-09-12 19:39:36 +0530

    feat: add Cloudinary upload function with progress tracking

---
+++ client/src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const FILE_TYPE = {
+    image: 'image',
+    video: 'video'
+} as const;
+
+export const CLOUDINARY_FOLDERS = {
+    avatar: 'voltex/avatars',
+    cover: 'voltex/covers',
+} as const;
