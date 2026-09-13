70b43202c732ef7055f6b7f999cef56d0fcead51
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-09-13 18:11:40 +0530

    feat: enhance avatar update logic with auth state management

--- client/src/features/profile/hooks/useUpdateAvatar.ts
+++ client/src/features/profile/hooks/useUpdateAvatar.ts
@@ -2,10 +2,13 @@ import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { updateAvatar } from "../api/profileApi"
 import { profileKeys } from "../api/profileKeys";
 import { authKeys, type AuthUser } from "@/features/auth";
+import { useAuthStore } from "@/store";
 
 export const useUpdateAvatar = (username: string) => {
 
     const queryClient = useQueryClient();
+    const auth = useAuthStore((state) => state.auth);
+    const setAuth = useAuthStore((state) => state.setAuth);
 
     return useMutation({
         mutationFn: updateAvatar,
@@ -27,6 +30,10 @@ export const useUpdateAvatar = (username: string) => {
 
                 return { ...oldData, profile: updatedProfile };
             });
+
+            if (auth) {
+                setAuth({ ...auth, profile: updatedProfile });
+            };
         },
     });
 };
