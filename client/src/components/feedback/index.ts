d6c0ba920ef0c388b3f30a9c2dd7129bfcac2ad7
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-09-24 09:29:28 +0530

    feat: add SentinelLoadingItem component for loading state indication

--- client/src/components/feedback/index.ts
+++ client/src/components/feedback/index.ts
@@ -1,2 +1,3 @@
 export { default as ErrorFallback } from "./ErrorFallback.tsx";
 export { default as Skeleton } from "./Skeleton.tsx";
+export { default as SentinelLoadingItem } from "./SentinelLoadingItem.tsx";
